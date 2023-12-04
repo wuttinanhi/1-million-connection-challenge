f0e02b1f4699ae0634c738554cfd16c3a26354ac
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2023-12-04 11:09:23 +0700

    update deploy and scripts

--- swarm_remove_stack.sh
+++ swarm_remove_stack.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# this script will run load test on swarm cluster
+
 # Get manager IP from Terraform output as JSON-like string
 manager_ips_json=$(terraform output -json manager_ip)
 
@@ -11,4 +13,4 @@ manager_external_ip=${manager_ips_array[0]}
 vm_username="docker"
 vm_password="@test12345"
 
-sshpass -p "$vm_password" ssh -o StrictHostKeyChecking=no $vm_username@$manager_external_ip "git clone https://github.com/wuttinanhi/1-million-connection-challenge.git && cd 1-million-connection-challenge && docker build -t 1m-connection . && docker stack deploy -c docker-compose.yml my-stack"
+sshpass -p "$vm_password" ssh -o StrictHostKeyChecking=no $vm_username@$manager_external_ip "docker stack rm my-stack"
